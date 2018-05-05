d527b48bc5343b71aa8c75318bceecbf029f9ed6
seattlechem <seattlechem@yahoo.com>
2018-05-04 20:35:17 -0700

    add_two_numbers scaffold

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # codewars-python
 codewars in python
+
+- Add Two Numbers [Link](https://leetcode.com/problems/add-two-numbers/description/)
---
+++ comfortable-words/solution.py
@@ -0,0 +1,19 @@
+LEFT = set('qwertasdfgzxcvb')
+RIGHT = set('yuiophjklnm')
+
+
+def comfortable_word(word):
+    one = []
+    two = []
+    for i, a in enumerate(word):
+        if not i % 2:
+            one.append(a)
+        else:
+            two.append(a)
+    one = ''.join(one)
+    two = ''.join(two)
+    return LEFT.issuperset(one) and RIGHT.issuperset(two) or \
+        LEFT.issuperset(two) and RIGHT.issuperset(one)
+
+
+print(comfortable_word('test'))
