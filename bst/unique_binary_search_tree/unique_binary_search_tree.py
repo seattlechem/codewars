98c8dd2a26d1ee016feb0d16b9af04bd6b2db4b3
seattlechem <seattlechem@yahoo.com>
2018-05-07 23:22:59 -0700

    num_trees implemented

--- bst/unique_binary_search_tree/unique_binary_search_tree.py
+++ bst/unique_binary_search_tree/unique_binary_search_tree.py
@@ -4,7 +4,7 @@
 def num_trees(n):
     """Specify how many structurally unique BST that store values 1..n."""
     dp = [1 for _ in range(n + 1)]
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     for i in range(2, n + 1):
         s = 0
         for j in range(i):
