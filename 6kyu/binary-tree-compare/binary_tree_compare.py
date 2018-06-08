18ed6acd56a1a3a50c555c12ee8b8c338d1cf361
seattlechem <seattlechem@yahoo.com>
2018-06-08 12:37:49 -0700

    other user's solution added

--- 6kyu/binary-tree-compare/binary_tree_compare.py
+++ 6kyu/binary-tree-compare/binary_tree_compare.py
@@ -1,11 +1,17 @@
 """Compare if two binary trees are identical."""
 
 
+# def compare(a, b):
+#     """Compare two binary trees to determine if they are identical."""
+#     if a is None and b is None:
+#         return True
+#     if a is not None and b is not None:
+#         return (a.val == b.val) and compare(a.left, b.left)\
+#                                 and compare(a.right, b.right)
+#     return False
+
+
 def compare(a, b):
-    """Compare two binary trees to determine if they are identical."""
-    if a is None and b is None:
-        return True
-    if a is not None and b is not None:
-        return (a.val == b.val) and compare(a.left, b.left)\
-                                and compare(a.right, b.right)
-    return False
+    """Other suggested method."""
+    return (a.val == b.val) and compare(a.left, b.left)\
+        and compare(a.right, b.right) if a and b else a == b
