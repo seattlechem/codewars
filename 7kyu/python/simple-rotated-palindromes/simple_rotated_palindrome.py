a39ba4862e31e1bc58ec5cb1bb13e87c852c4010
Peter Kim <seattlechem@yahoo.com>
2018-06-13 13:10:35 -0700

    best practice solution added

--- 7kyu/python/simple-rotated-palindromes/simple_rotated_palindrome.py
+++ 7kyu/python/simple-rotated-palindromes/simple_rotated_palindrome.py
@@ -8,10 +8,15 @@ def solve(st):
     if is_palindrome(st) is True:
         return True
     st_list = list(str(st))
-    for i in range(0, len(st_list) - 1):
+    for i in range(0, len(st_list)):
         last = st_list.pop()
         st_list.insert(0, last)
         st = ''.join(st_list)
         if is_palindrome(st) is True:
             return True
     return False
+
+
+def best_solve(s):
+    """Best practice solution from codewars."""
+    return any(s[i+1:] + s[:i+1] == s[i::-1] + s[:i:-1] for i in range(len(s)))
