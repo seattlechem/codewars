75fc50100e25caba3eefb5247aa23a74fcfaa0be
seattlechem <seattlechem@yahoo.com>
2018-06-19 16:08:45 -0700

    solution and test complete

---
+++ leet-code/lowest-ancestor-bst/test_lca_bst.py
@@ -0,0 +1,16 @@
+"""Test cases."""
+from lca_bst import lca_bst
+
+
+class Node:
+    """Node class declared."""
+
+    def __init__(self, val):
+        """Instance of Node class."""
+        self.val = val
+
+
+def test_true(first_bst):
+    """Test true."""
+    result = lca_bst(first_bst, Node(2), Node(8))
+    assert result.val == 6
