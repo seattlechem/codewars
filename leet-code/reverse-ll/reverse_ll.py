f9457c7e87fd3ca32465216df80c8c16068ff82f
seattlechem <seattlechem@yahoo.com>
2018-06-25 20:57:22 -0700

    solution / test complete

--- leet-code/reverse-ll/reverse_ll.py
+++ leet-code/reverse-ll/reverse_ll.py
@@ -16,23 +16,36 @@ class LinkedList:
     def __init__(self):
         """Instance of linked list is created."""
         self.head = None
+        self.length = 0
 
     def push(self, val):
         """Insert a new node at the beginning."""
         new_node = Node(val)
         new_node.next = self.head
         self.head = new_node
+        self.length += 1
 
     def reverse_iterative(self):
         """Reverse a singly linked list in an iterative way."""
         """O(n) / O(1) solution."""
-        # import pdb; pdb.set_trace()
         pre = None
         current = self.head
-
         while current is not None:
             next = current.next
             current.next = pre
-            prev = current
+            pre = current
             current = next
-        self.head = prev
+        self.head = pre
+
+    def reverse_recur(self):
+        """O(n) / O(1)."""
+        def _reverse_helper(current, pre):
+            """Recursive helper function."""
+            if current is None:
+                return pre
+            next = current.next
+            current.next = pre
+            pre = current
+            current = next
+            return _reverse_helper(current, pre)
+        self.head = _reverse_helper(current=self.head, pre=None)
--- leet-code/reverse-ll/test_reverse_ll.py
+++ leet-code/reverse-ll/test_reverse_ll.py
@@ -5,6 +5,7 @@ import pytest
 
 @pytest.fixture
 def simple_ll():
+    """Create a simple linked list."""
     ll = LinkedList()
     ll.push(20)
     ll.push(4)
@@ -17,6 +18,13 @@ def test_true(simple_ll):
     """True test case."""
     assert simple_ll.head.val == 85
     assert isinstance(simple_ll, LinkedList) is True
-    # import pdb; pdb.set_trace()
     simple_ll.reverse_iterative()
     assert simple_ll.head.val == 20
+    assert simple_ll.length == 4
+    assert simple_ll.head.next.val == 4
+    assert isinstance(simple_ll, LinkedList) is True
+    simple_ll.reverse_recur()
+    assert isinstance(simple_ll, LinkedList) is True
+    assert simple_ll.head.val == 85
+    assert simple_ll.head.next.val == 15
+    assert simple_ll.head.next.next.val == 4
