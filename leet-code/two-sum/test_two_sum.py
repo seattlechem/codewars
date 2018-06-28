f15e3cbe79c6ff893e266ef97d16d2d42d21df05
Peter Kim <seattlechem@yahoo.com>
2018-06-28 11:35:51 -0700

    method using dict added

--- leet-code/two-sum/test_two_sum.py
+++ leet-code/two-sum/test_two_sum.py
@@ -1,7 +1,8 @@
 """Test cases."""
-from two_sum import two_sum
+from two_sum import two_sum, two_sum_dict
 
 
 def test_true():
     """True case test."""
     assert two_sum([1, 2, 5, 6, 7], 9) == [1, 4]
+    assert two_sum_dict([1, 2, 5, 6, 7], 9) == [1, 4]
--- leet-code/two-sum/two_sum.py
+++ leet-code/two-sum/two_sum.py
@@ -7,10 +7,27 @@ def two_sum(arr, target):
 
     :input: array and target value
     :output: array (containing two indices)
-    :Time: O(n)
+    :Time: O(n2)
     :Space: O(1)
     """
     for i in range(len(arr)):
         for j in range(i + 1, len(arr)):
             if arr[i] + arr[j] == target:
                 return [i, j]
+
+
+def two_sum_dict(arr, target):
+    """
+    Increase speed by using dictionary.
+
+    :Time: O(n)
+    :Space: O(n)
+    """
+    re = {}
+    for i in range(len(arr)):
+        re[arr[i]] = i
+
+    for i in range(len(arr)):
+        diff = target - arr[i]
+        if diff in re:
+            return [i, re[diff]]
