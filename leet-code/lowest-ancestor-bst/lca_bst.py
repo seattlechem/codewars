fecb0093141e28986f938faef4c558f337785306
seattlechem <seattlechem@yahoo.com>
2018-09-05 23:01:22 -0700

    readme updated | methods added

--- leet-code/lowest-ancestor-bst/lca_bst.py
+++ leet-code/lowest-ancestor-bst/lca_bst.py
@@ -11,7 +11,7 @@ class TreeNode:
         self.right = None
 
 
-def lca_bst(root, p, q):
+def lca_bst_recursive(root, p, q):
     """Lowest common ancestor of two given nodes in BST."""
     if root.val > max(p.val, q.val):
         return lca_bst(root.left, p, q)
@@ -21,3 +21,27 @@ def lca_bst(root, p, q):
 
     else:
         return root
+
+
+
+def lowestCommonAncestor(root, p, q):
+    """O(n) solution."""
+    while root:
+        if p.val < root.val > q.val:
+            # p and q smaller, mean they locate left
+            root = root.left
+        elif p.val > root.val < q.val:
+            root = root.right
+        else:
+            # otherwise the lowest common ancestor is root
+            return root
+
+
+def lca_three_lines(root, p, q):
+    while (root.val - p.val) * (root.val - q.val) > 0:
+        #this means either both p and q are smaller or greater than root
+        root = (root.left, root.right)[p.val > root.val]
+        # it could be q.val, but in this case p was chosen
+        # if p is greater than root.val (either True (1) False(0))
+        # root.left or root.right is chosen depends on index (1 or 0)
+    return root
